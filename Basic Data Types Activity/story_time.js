2717ff3adfa81293c58a211938e97469aa8438f6
TheRealRehan <111456871+TheRealRehan@users.noreply.github.com>
2022-08-19 11:53:54 -0500

    submitting basic data types file

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,33 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greetings = "Welcome to Cadet Village!";
+let name = "Mandolorian";
+let title = "Master Bounty Hunter";
+let worldName = "Tatowien";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let age = 3500;
+let year = 2098;
+let century = 430;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let weaponsUsed = ["DarkSaber", "Blaster", "Jetpack", "Xbox Game Pass"];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let hasFavWeapon = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(greetings + " My name is " + name + ".");
+console.log("Im the " + title);
+console.log("As the leader of " + worldName + ", I use different weapons.");
+console.log("Time works differently in my world");
+console.log("For example, I'm" + age + " years old");
+console.log("In my world, you are in year " + year + ", the " + century + " the century." + "\n");
+console.log("Does anyone have any questions?");
+console.log("Me: Do you have a favorite weapon?");
+console.log("Hunter: " + hasFavWeapon + ", my favorite weapon is " + weaponsUsed[4] + "." + "\n");
+console.log("You are free to explore my world and return to Earth after your tour is over!");
